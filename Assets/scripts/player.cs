38b5f4cdd27e66937eaffc54bc344e5ffb398eaa
Yamilah Atallah <yami@yamilah.com>
2018-05-28 01:43:33 -0700

    Initial commit.

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public float size = 1;
+
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	private void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Water") {
+			size += 0.01f;
+		}
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Cloud") {
+			Destroy(other.gameObject);
+			size += 1;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(size, 1, size), .2f);
+
+		transform.position += new Vector3(x, 0, y) * Time.fixedDeltaTime * speed;
+	}
+}
---
+++ Assets/scripts/puddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puddle : MonoBehaviour {
+
+	public float water = 10;
+
+	private void OnTriggerStay(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			water -= 0.05f;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (water <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
